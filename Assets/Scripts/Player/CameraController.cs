bd20f529a4a1c217fb7e0c15b71276cd7905ce2b
PizzaMan730 <ajarchie730@gmail.com>
2025-02-16 16:05:19 -0500

    Oopsie, I forgot to adjust some values

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -4,16 +4,23 @@ public class CameraController : MonoBehaviour
 {
     private Camera cam;
     private PlayerController player;
+    private float baseRotationY;
+    private Vector3 rotationVariance;
     void Awake()
     {
         cam = GetComponent<Camera>();
         player = transform.parent.GetComponent<PlayerController>();
+        baseRotationY = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        cam.transform.localEulerAngles = new Vector3(ClampAngle(cam.transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * player.mouseSensitivity), cam.transform.localEulerAngles.y, cam.transform.localEulerAngles.z);
+        bool walking = Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0;
+        bool sprinting = Input.GetKeyDown(KeyCode.LeftShift) && walking;
+        baseRotationY = ClampAngle(baseRotationY - Input.GetAxis("Mouse Y") * player.mouseSensitivity);
+        cam.transform.localEulerAngles = new Vector3(baseRotationY, 0f, 0f);
+
     }
         public static float ClampAngle(float angle) {
         if (angle > 180)
