a17d7fb16b45f507ce86c728c6949964ed96bf43
NonzeroCornet34 <ullerbri000@gmail.com>
2025-02-17 16:35:56 -0700

    Networking go BRRRRR

--- Assets/VampireTCP/MultiplayerManager.cs
+++ Assets/VampireTCP/MultiplayerManager.cs
@@ -1,11 +1,19 @@
+using Newtonsoft.Json;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public class PositionData
+{
+    public string t { get; set; }
+}
+
 public class MultiplayerManager : MonoBehaviour
 {
     [SerializeField] private VampireTCP networkManager;
+
+    public GameObject newPlayerInstance;
 
     public void HostGame()
     {
@@ -24,8 +32,37 @@ public class MultiplayerManager : MonoBehaviour
         UnityEngine.Application.Quit();
     }
 
+    Vector3 StringToVector3(string input)
+    {
+        input = input.Trim('(', ')'); // Remove parentheses
+        string[] values = input.Split(',');
+
+        if (values.Length == 3 &&
+            float.TryParse(values[0], out float x) &&
+            float.TryParse(values[1], out float y) &&
+            float.TryParse(values[2], out float z))
+        {
+            return new Vector3(x, y, z);
+        }
+
+        Debug.LogError("Invalid format: " + input);
+        return Vector3.zero;
+    }
+
     public void OnRecieveNewMessage(MessageWrapper wrapper)
     {
-        Debug.Log(wrapper);
+        if(GameObject.Find("Player" +wrapper.msg.from))
+        {
+            if (wrapper.msg.message == "updatePlayerPosition")
+            {
+                PositionData posData = JsonConvert.DeserializeObject<PositionData>(wrapper.msg.value.ToString());
+
+                GameObject.Find("Player" + wrapper.msg.from).transform.position = StringToVector3(posData.t);
+            }
+        } else
+        {
+            GameObject newPlayer = Instantiate(newPlayerInstance);
+            newPlayer.name = "Player" + wrapper.msg.from;
+        }
     }
 }
